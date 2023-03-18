7de4e753cde6bb2f08b4448988217740e036a9e3
jotavo13 <juandotavo@gmail.com>
2023-03-18 15:23:49 -0400

    wokring model so far

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 //first to init express ---- import express
 const express = require('express');
+const app = express();
+
+const drinks = require('./models/Drinks');
+// console.log(models);
+
+
 
 // Models - Database stuff
 // controllers - routes
 // views - EJS files (EJS is literally just HTML and JS)
 
 // new instance of express
-const app = express();
-
 const port = 3000;
 
 
 
+// const drinks = require('drink');
+
 // Middleware 
 // This is a view engine that is looking for EJS files to be rendered. It also sets up that ALL the EJS files for my frontend will be located in a file named views
-app.set('view engine', 'ejs')
-
-
-
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!')
+    res.send('Welcome to the Gitpub App!');
 }) 
 
 // Routes
 app.get('/drinks', (req, res) => {
-    res.render('index.ejs');
+    res.render('index', {drinks});
 })
+
 
 
 
